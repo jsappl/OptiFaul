bed7c48994b27f17b1ec15700b7ad822baeff499
Johannes Sappl <johannes.sappl@chello.at>
2022-10-09 13:46:59 +0200

    feat: add trainer with hydra

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 """Train and test temporal fusion transformers with anaerobic digester data."""
 
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 import hydra
-import pytorch_lightning as pl
+import pytorch_lightning
 from hydra.utils import instantiate
 
 from optifaul.utils import namestr_from
@@ -15,8 +14,7 @@ if TYPE_CHECKING:
 
 @hydra.main(config_path="config", config_name="config", version_base="1.2")
 def main(cfg: "DictConfig") -> None:
-    pl.seed_everything(cfg.seed, workers=True)
-    save_dir = Path(hydra.core.hydra_config.HydraConfig.get()["runtime"]["output_dir"])
+    pytorch_lightning.seed_everything(cfg.seed, workers=True)
 
     if cfg.run_etl:
         etl = instantiate(cfg.etl)
@@ -36,6 +34,12 @@ def main(cfg: "DictConfig") -> None:
         loss=loss,
     )
     logger = instantiate(cfg.logging, version=namestr_from(model))
+    trainer = instantiate(
+        cfg.trainer,
+        logger=logger,
+        callbacks=[],
+    )
+    trainer.fit(model, train_loader, val_loader)
 
 
 if __name__ == "__main__":
