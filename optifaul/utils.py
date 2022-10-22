ee1b8c2be85bd6eb01b65f6e80320c6bf30cf224
Johannes Sappl <johannes.sappl@chello.at>
2022-10-22 21:02:01 +0200

    feat: r2 metric function :sparkles:

--- optifaul/utils.py
+++ optifaul/utils.py
@@ -4,9 +4,11 @@ from datetime import datetime
 from typing import TYPE_CHECKING, Type
 
 import pandas as pd
+import numpy as np
 
 if TYPE_CHECKING:
     from pandas import Series
+    from numpy import ndarray
 
 
 def new_headers() -> list:
@@ -56,3 +58,19 @@ def get_time_series(file_: str, dates: "Series") -> "Series":
 def namestr_from(_class: "Type") -> str:
     """Extract name string from class instance."""
     return _class.__class__.__name__
+
+
+def r_squared(targets: "ndarray", predictions: "ndarray") -> float:
+    """Calculate the coefficient of determination.
+
+    Args:
+        targets: The ground truth.
+        predictions: The model output.
+
+    Returns:
+        The coefficient of determination, also called R^2.
+    """
+    idx = np.squeeze(np.argwhere(~np.isnan(predictions)))
+    corr_matrix = np.corrcoef(targets[idx], predictions[idx])
+    corr = corr_matrix[0, 1]
+    return corr**2
